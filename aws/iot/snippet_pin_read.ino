80bc1aa0398db4fe123838c3c007ffe3047f2fea
Robert Pyke <robert.j.pyke@gmail.com>
2018-04-02 00:32:29 -0700

    Update snippet_pin_read.ino

--- aws/iot/snippet_pin_read.ino
+++ aws/iot/snippet_pin_read.ino
@@ -1,4 +1,5 @@
-int touch_sensor_pin = 7
+// The pin we'll attach the touch-sensor to (in addition to VCC/Ground)
+int touch_sensor_pin = 0
 setup() {
   // Read the sensor
   int pin = digitalRead(touch_sensor_pin);
