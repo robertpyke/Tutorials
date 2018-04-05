463723b1f1ab18e9c2c3302dd4e37e544b8beb2f
Robert Pyke <robert.j.pyke@gmail.com>
2018-04-04 20:17:07 -0700

    Create snippet_send_mqtt.ino

---
+++ aws/iot/snippet_send_mqtt.ino
@@ -0,0 +1,13 @@
+char publishPayload[MQTT_MAX_PACKET_SIZE];
+char publishTopic[]   = "$aws/things/" THING_NAME "/shadow/update";
+
+void setup() {
+// Assuming client is an mqtt client.
+sprintf(publishPayload, "{\"state\":{\"reported\":{\"led\":%d }},\"clientToken\":\"%s\"}",
+          digitalRead(LED_BUILTIN),
+          WiFi.RSSI(),
+          digitalRead(sound),
+          clientId
+         );
+  client.publish(publishTopic, publishPayload);
+}
