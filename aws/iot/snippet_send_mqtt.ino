e64db172a3a8649c77e489ba2773c6ccc54a1fa7
Robert Pyke <robert.j.pyke@gmail.com>
2018-04-04 20:17:29 -0700

    Update snippet_send_mqtt.ino

--- aws/iot/snippet_send_mqtt.ino
+++ aws/iot/snippet_send_mqtt.ino
@@ -5,8 +5,6 @@ void setup() {
 // Assuming client is an mqtt client.
 sprintf(publishPayload, "{\"state\":{\"reported\":{\"led\":%d }},\"clientToken\":\"%s\"}",
           digitalRead(LED_BUILTIN),
-          WiFi.RSSI(),
-          digitalRead(sound),
           clientId
          );
   client.publish(publishTopic, publishPayload);
